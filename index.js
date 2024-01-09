98c8de9d1758265229a2a91ddf3e88d98835d8df
Chris Duxbury <chris_duxbury@outlook.com>
2024-01-09 23:33:01 +0800

    Updated email post logic

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
+const Mailgen = require("mailgen");
+require('dotenv').config();
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.post("/emails", (req, res) => {
-    res.send("Hello Emails!");
+    let config = {
+        service: 'gmail',
+        auth: {
+            user: process.env.GMAIL_APP_USER,
+            pass: process.env.GMAIL_APP_PASSWORD
+        }
+    };
+    let transporter = nodemailer.createTransport(config);
+
+    let message = {
+        from: 'duxcrispy@gmail.com',
+        to: req.body.to,
+        subject: req.body.subject,
+        text: req.body.body,
+    };
+
+    transporter.sendMail(message).then((info) => {
+        return res.status(201).json(
+            {
+                msg: "Email sent",
+                info: info.messageId,
+                preview: nodemailer.getTestMessageUrl(info)
+            }
+        )
+    }).catch((err) => {
+        return res.status(500).json({ msg: err });
+    });
 });
 
 app.listen(port, () => {
